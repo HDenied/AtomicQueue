f0bcfacc04fe5ae116453c1e96d0dcb925e5d20c
simone greco <simone.grecouk@gmail.com>
2022-09-11 09:41:53 +0100

    monotonic queue single thread

--- mqueue.h
+++ mqueue.h
@@ -1,41 +1,68 @@
 #pragma once
+#include "atlock.hpp"
 #include <atomic>
+#include <iostream>
+#include <limits.h>
+#include <memory>
 #include <mutex>
 #include <stdint.h>
+#include <thread>
+#include <unistd.h>
+#include <utility> 
 
-template <class T, uint32_t N = 100> class Mqueue {
-  T el[N];
-  std::mutex id_guard;
-  std::uint8_t id_shift = 0;
+template <class T, int32_t MAX_SIZE = UCHAR_MAX> class MtQueue {
+protected:
+  T _q[MAX_SIZE]{};
 
-  std::atomic<uint8_t> thq_id{};
-
-  uint32_t wr_idx = 0;
-  uint32_t rd_idx = 0;
-
-  std::atomic<uint8_t> q_guard{};
+  int32_t _front{-1};
+  int32_t _b{0};
 
 public:
-  enum S_TYPE : uint8_t { FREE = 0, B_READER = 1, B_WRITER = 2 };
-  bool is_supported() const { return std::atomic_is_lock_free(&thq_id); }
+  MtQueue() = default;
 
-  bool get_id(uint8_t &id) {
-    std::unique_lock<std::mutex> lck(id_guard);
+  bool push(T &&obj) {
+    int32_t pos = 0;
+    if ((pos = _front) < MAX_SIZE) {
 
-    if (id_shift < 8) {
-      id = 1 << id_shift;
-      return false;
-    } else {
+      if (pos != -1) {
+        _q[pos] = obj;
+        pos++;
+      } else {
+        _q[0] = obj;
+        pos = 1;
+      }
+
+      _front=pos;
+      // still room to write
       return true;
     }
-  }
-
-  S_TYPE front(const T **res) {
-    if (q_guard.load(std::memory_order_acquire) != S_TYPE::B_WRITER) {
-      *res = &(el[rd_idx]);
-      return S_TYPE::FREE;
-    } else {
-      return S_TYPE::B_WRITER;
+    // Writer gets at the end of the queue
+    else {
+      return false;
     }
   }
-};
+
+  bool pop(T &elem) {
+    int32_t end = 0;
+    int32_t start = _b.load();
+    if ((end = _front) >= 0) {
+      if (((end < MAX_SIZE || start < end) && start != end) || end == 0) {
+        elem = _q[start];
+        _b=start + 1;
+
+        return true;
+      } else if (end == MAX_SIZE && start == end) {
+        _b=0;
+        _front=-1;
+        return false;
+      } else {
+        return false;
+      }
+    }
+    // Not yet started any write
+    else {
+      return false;
+    }
+  }
+}; // namespace cpputils
+
