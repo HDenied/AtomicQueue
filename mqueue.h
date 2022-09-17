6f5213122d4c79813c0d896f52292ea8119f0bbd
simone greco <simone.grecouk@gmail.com>
2022-09-17 09:47:24 +0100

    dual multithreaded queue

--- mqueue.h
+++ mqueue.h
@@ -64,5 +64,60 @@ public:
       return false;
     }
   }
-}; // namespace cpputils
+}; 
 
+template <class T, int32_t MAX_SIZE = UCHAR_MAX> class MtQueue {
+protected:
+  T _q[MAX_SIZE]{};
+
+  std::atomic<int32_t> _front{-1};
+  std::atomic<int32_t> _b{0};
+
+public:
+  MtQueue() = default;
+
+  bool push(T &&obj) {
+    int32_t pos = 0;
+    if ((pos = _front.load(std::memory_order_acquire)) < MAX_SIZE) {
+
+      if (pos != -1) {
+        _q[pos] = obj;
+        pos++;
+      } else {
+        _q[0] = obj;
+        pos = 1;
+      }
+
+      _front.store(pos, std::memory_order_release);
+      // still room to write
+      return true;
+    }
+    // Writer gets at the end of the queue
+    else {
+      return false;
+    }
+  }
+
+  bool pop(T &elem) {
+    int32_t end = 0;
+    int32_t start = _b.load();
+    if ((end = _front.load(std::memory_order_acquire)) >= 0) {
+      if (((end < MAX_SIZE || start < end) && start != end) || end == 0) {
+        elem = _q[start];
+        _b.store(start + 1, std::memory_order_release);
+
+        return true;
+      } else if (end == MAX_SIZE && start == end) {
+        _b.store(0, std::memory_order_release);
+        _front.store(-1, std::memory_order_acquire);
+        return false;
+      } else {
+        return false;
+      }
+    }
+    // Not yet started any write
+    else {
+      return false;
+    }
+  }
+};
